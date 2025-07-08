72308c7e65d8386c349511c61329545d17aec5be
YASHMITHA A <yashmithavagman@gmail.com>
2025-07-08 22:11:57 +0530

    updated

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:todotute/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:Scaffold(),
+      home: HomePage(),
     );
   }
 }
